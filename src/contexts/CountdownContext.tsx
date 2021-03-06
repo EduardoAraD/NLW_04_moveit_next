2d7286ea3edd955692d390cd75078232aa8dadf6
EduardoAraD <araujocarlos893@gmail.com>
2021-03-06 07:08:36 -0300

    deploy vercel

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -19,7 +19,7 @@ const CountdownContext = createContext({} as CountdownContextData)
 let countdownTimeout: NodeJS.Timeout;
 export function CountdownProvider({children}: CountdownContextProps) {
     const { startNewChallenge } = useChallengeContext()
-    const [time, setTime] = useState(0.2 * 60);
+    const [time, setTime] = useState(25 * 60);
     const [isActive, setIsActive] = useState(false);
     const [hasFinished, setHasFinished] = useState(false);
 
@@ -34,7 +34,7 @@ export function CountdownProvider({children}: CountdownContextProps) {
         clearTimeout(countdownTimeout);
         setIsActive(false);
         setHasFinished(false);
-        setTime(0.2 * 60);
+        setTime(25 * 60);
     }
 
     useEffect(() => {
